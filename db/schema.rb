c595b9b23c368c808a8bb6817aadfe2ddd3d30dd
CharlieAllen <miss.neophyte@gmail.com>
2016-11-05 21:14:51 +0000

    user model and controller generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105192841) do
+ActiveRecord::Schema.define(version: 20161105211041) do
 
   create_table "partners", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
